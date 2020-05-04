0dc611e7ed5a4387afda7aa59ccfb6d3fc611dfe
Ted Steinmann <tedsteinmann@gmail.com>
2020-05-04 18:55:58 -0500

    print df of orders

--- src/main.py
+++ src/main.py
@@ -2,5 +2,10 @@ from classes.reader import Read
 from classes.writer import Write
 
 orders = Read('amazon-orders.csv')
-order_count = orders.df.shape[0]
-print("Orders: ",order_count)
+df = orders.df
+
+df['Total'] = (df['Item Total'].replace( '[\$,)]','', regex=True ).replace( '[(]','-',   regex=True ).astype(float))
+
+orders_grouped = df.groupby('Category').agg({'Total':['sum','count']}).reset_index().sort_values(by='Category',ascending=False)
+
+print(orders_grouped)
