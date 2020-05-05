f767574cfba6eb347c2a29d97e403405e8facc38
Ted Steinmann <tedsteinmann@gmail.com>
2020-05-04 22:34:32 -0500

    monthly budget and cleanup

--- src/main.py
+++ src/main.py
@@ -2,25 +2,56 @@ from classes.reader import Read
 from classes.writer import Write
 from classes.mapper import Map
 
+import pandas as pd
+
+#READ ORDERS
+#----------------------
 orders = Read('amazon-orders.csv')
-df = orders.df #['Order Date','Category','Quantity','Item Total']
-df = df[['Order Date','Category','Quantity','Item Total']].copy(deep=False)
+orders_df = orders.df #['Order Date','Category','Quantity','Item Total']
+orders_df = orders_df[['Order Date','Category','Quantity','Item Total']].copy(deep=False)
 #print(df.head())
 
-df['Total'] = (df['Item Total'].replace( '[\$,)]','', regex=True ).astype(float))
+# DFINE CATEGORY MAPPINGS
+#----------------------
+mapper = Map('categories.csv','Category')
 
-orders_grouped = df.groupby(['Category'],sort=True).sum().reset_index()
+
+# REMOVE $ CHARACTER
+#----------------------
+orders_df['Total'] = (orders_df['Item Total'].replace( '[\$,)]','', regex=True ).astype(float))
+
+
+# ORDERS BY MONTHLY SPEND
+#----------------------
+mapped_orders = mapper.map_data(orders_df,'Category')
+
+# make a month column to preserve the order
+mapped_orders['month'] = pd.to_datetime(mapped_orders['Order Date']).dt.strftime('%m')
+
+# create the pivot table with this numeric month column
+orders_df_pivot = mapped_orders.pivot_table(index='month',columns=['Parent Category'],values=['Total'],aggfunc=sum, fill_value=0).T
+
+# print(orders_df_pivot)
+
+Write(orders_df_pivot,'orders_by_monthly_spend')
+
+
+# ORDERS GROUPED BY CATEGORY
+#-------------------
+orders_grouped = orders_df.groupby(['Category'],sort=True).sum().reset_index()
+
 orders_grouped = orders_grouped.sort_values(by=['Total','Quantity'], ascending=[False,False])
 
 #print(orders_grouped)
-Write(orders_grouped,'orders_grouped')
+Write(orders_grouped,'orders_by_category')
 
-mapper = Map('categories.csv','Category')
+# ORDERS GROUPED BY PARENT CATEGORY
+#----------------------
 
-mapped_orders = mapper.map_data(orders_grouped,'Category')
+mapped_grouped_orders = mapper.map_data(orders_grouped,'Category')
+
+mapped_grouped_orders = mapped_grouped_orders.groupby(['Parent Category'],sort=True).sum().reset_index()
+mapped_grouped_orders = mapped_grouped_orders.sort_values(by=['Total','Quantity'], ascending=[False,False])
 
 #print(mapped_orders)
-Write(mapped_orders,'mapped_orders')
-
-# make a month column to preserve the order
-# mapped_orders['month'] = pd.to_datetime(df['date']).dt.strftime('%m')
+Write(mapped_grouped_orders,'orders_by_parent_category')
